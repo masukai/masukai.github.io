5f5a7a52364d941af69c94cbaf6819d21e421ca9
KaitoMasuda <37993351+masukai@users.noreply.github.com>
2021-01-17 17:24:53 +0900

    2021_01_17_update

--- dice.js
+++ dice.js
@@ -1,23 +1,37 @@
 var count; // 変化しているように見せる回数
+let result = [];
+let result_count = -1;
 var $id = function (id) {
     return document.getElementById(id);
 };
 
 function roll() {
     var sai = Math.floor(Math.random() * 6) + 1; // 1から6までの適当な数字
-    sai = sai + ".png"; // 画像ファイル名生成
-    $id("saikoro").innerHTML = "<img src='dice_photo/" + sai + "' width='256' height='256'>";
+    $id("saikoro").innerHTML = "<img src='dice_photo/" + sai + ".png' width='256' height='256'>";
     //$id("saikoro2").innerHTML = "<img src='dice_photo/" + sai + "' width='256' height='256'>";
+    return sai;
 }
 
 function anime() {
-    if (count > 15) { // 適当に20回ほど振る
+    let sai = roll();
+    count++;
+    if (count > 15) {
         count = 0;
+        result_count++;
+        result.push(sai);
+        console.log(result);
         $id("btn").disabled = ""; // ボタンを使える状態にする
+        if (result_count > 0) {
+            $id("result1").innerHTML = "<img src='dice_photo/" + result[result_count - 1] + ".png' width='128' height='128'>";
+        }
+        if (result_count > 1) {
+            $id("result2").innerHTML = "<img src='dice_photo/" + result[result_count - 2] + ".png' width='128' height='128'>";
+        }
+        if (result_count > 2) {
+            $id("result3").innerHTML = "<img src='dice_photo/" + result[result_count - 3] + ".png' width='128' height='128'>";
+        }
         return 0;
     }
-    roll();
-    count++;
     setTimeout(anime, 50); // 50ミリ秒間隔で表示切り替え
 }
 
@@ -28,5 +42,5 @@ function saikoro() {
 }
 
 window.onload = function () {
-    roll(); // 一回だけサイコロを振る
+    let sai = roll(); // 一回だけサイコロを振る
 }
